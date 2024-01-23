17e1d94d13eeb9928b518eedf1a2cab7b51ea73b
SquallStrifeX <squallstrifex@gmail.com>
2024-01-23 22:56:22 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-6"> 
+<form action="result.php" method="GET">
+  <div class="mb-3">
+    <label for="control-label" class="form-label">Inserisci un testo</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" name="testo">
+  </div>
+  <div class="mb-3">
+    <label for="control-label" class="form-label">Inserisci la parola da censurare</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" name ="censura">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+</div>
+    </div>
+</body>
+</html>
